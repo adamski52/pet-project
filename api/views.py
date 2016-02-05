bec93e169ff4979942da8eb1c4d9bd5cf9e3b335
Jonathan Adamski <jon@Jonathans-Mac-mini.local>
2016-02-04 21:17:39 -0500

    jen ass authentication over rest.

--- api/views.py
+++ api/views.py
@@ -1,7 +1,23 @@
-from api.models import UserProfile, Family
 from rest_framework import viewsets
+from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
-from api.serializers import *
+from api.authentication import *
+from api.serializers import UserSerializer, UserProfileSerializer, FamilySerializer
+from api.models import UserProfile, Family
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class AuthView(APIView):
+    authentication_classes = (QuietBasicAuthentication,)
+    serializer_class = UserSerializer
+ 
+    def post(self, request, *args, **kwargs):
+        login(request, request.user)
+        return Response(self.serializer_class(request.user).data)
+
+    def delete(self, request, *args, **kwargs):
+        logout(request)
+        return Response({})
 
 
 class UserViewSet(viewsets.ModelViewSet):
